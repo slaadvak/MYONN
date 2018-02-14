45f2f97eb5fa189648b72459b3bb046d7a2e8570
Vincent Mercier <vincent.mercier@gmail.com>
2018-02-14 01:40:34 +0000

    Added save, load train_dataset and test_dataset methods

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-# python notebook for Make Your Own Neural Network
+# Make Your Own Neural Network
 # (c) Tariq Rashid, 2016
 # license is GPLv2
 
+# Added functionalities
+# (c) Vincent Mercier, 2018
+# license is GPLv2
+
 import numpy
+import Plot
 # scipy.special for the sigmoid function expit()
 import scipy.special
 
+
 # neural network class definition
 class NeuralNetwork:
-	
+    
     # initialise the neural network
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         # set number of nodes in each input, hidden, output layer
@@ -19,8 +25,8 @@ class NeuralNetwork:
         self.onodes = output_nodes
         
         self.lr = learning_rate
-		
-		# link weight matrices, wih and who (input and output weights of hidden layer nodes)
+        
+        # link weight matrices, wih and who (input and output weights of hidden layer nodes)
         # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
         # w11 w21
         # w12 w22 etc 
@@ -29,7 +35,7 @@ class NeuralNetwork:
         
         # activation function is the sigmoid function
         self.activation_function = lambda x: scipy.special.expit(x)
-		
+        
     def train(self, inputs_list, targets_list):
         # convert inputs list to 2d array
         inputs = numpy.array(inputs_list, ndmin=2).T
@@ -56,6 +62,22 @@ class NeuralNetwork:
         # update the weights for the links between the input and hidden layers
         self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
 
+    def query(self, inputs_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        
+        return final_outputs
+
     def train_dataset(self, data_file, epochs=1):
         records_cnt = 0
         for e in range(epochs):
@@ -74,20 +96,32 @@ class NeuralNetwork:
                     print("Epoch: {} ({})".format(e + 1, records_cnt), end='\r')
 
         print("Trained with a dataset of {} records for {} epochs.".format(int(records_cnt / epochs), epochs))
-		
-    def query(self, inputs_list):
-        # convert inputs list to 2d array
-        inputs = numpy.array(inputs_list, ndmin=2).T
         
-        # calculate signals into hidden layer
-        hidden_inputs = numpy.dot(self.wih, inputs)
-        # calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
+    def test_dataset(self, test_file, show_errors=False):
+        records_cnt = 0
+        good_cnt = 0
+        with open(test_file) as f:
+            for record in f:
+                target = int(record[0])
+                inputs = (numpy.asfarray(record.split(',')[1:]) / 255.0 * 0.99) + 0.01
+                outputs = self.query(inputs)
+                found = int(numpy.argmax(outputs))
+                records_cnt += 1
+                
+                if found == target:
+                    good_cnt += 1
+                elif show_errors:
+                    Plot.plot_record(inputs, target, outputs[found][0] * 100.0, found)
+                    
+        print("{} errors on {} records".format(records_cnt - good_cnt, records_cnt))
+        print("Accuracy of {}%".format(float(good_cnt) / float(records_cnt) * 100.0))
         
-        # calculate signals into final output layer
-        final_inputs = numpy.dot(self.who, hidden_outputs)
-        # calculate the signals emerging from final output layer
-        final_outputs = self.activation_function(final_inputs)
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            numpy.save(f, self.wih)
+            numpy.save(f, self.who)
         
-        return final_outputs
-		
+    def load(self, filename):
+        with open(filename, 'rb') as f:
+            self.wih = numpy.load(f)
+            self.who = numpy.load(f)
